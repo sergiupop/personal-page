b048e6ac43ef311229c124040716c63bf6136469
Sergiu Pop <sergiupop94@gmail.com>
2022-08-02 11:05:08 +0300

    Add initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <section id="section1" style={{ height: "2000px" }}>
+        SECTION 1
+      </section>
+
+      <section id="section2" style={{ height: "2000px" }}>
+        SECTION 2
+      </section>
+
+      <section id="section3" style={{ height: "2000px" }}>
+        SECTION 3
+      </section>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <a href="#section1" style={{ marginRight: "20px" }}>
+        Go to section 1
+      </a>
+      <a href="#section2" style={{ marginRight: "20px" }}>
+        Go to section 2
+      </a>
+      <a href="#section3">
+        Go to section 3
+      </a>
+    </nav>
+  )
+}
+
+export default Navbar;
