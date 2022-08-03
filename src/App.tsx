ec5913bfa4ac9e9e1734b77d0c18350fae2adac9
Sergiu Pop <sergiupop94@gmail.com>
2022-08-03 09:55:15 +0300

    Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 const App = () => {
   return (
     <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <Navbar />
       <section id="section1" style={{ height: "2000px" }}>
         SECTION 1
