3fd3e656af001e9628db3ba36fe7e30350ad383e
Sergiu Pop <sergiupop94@gmail.com>
2022-08-04 11:35:03 +0300

    Center elements within the navbar

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,5 +1,3 @@
-import "./Link.css";
-
 type LinkProps = {
   text: string;
   href: string;
@@ -9,7 +7,7 @@ const Link = ({ text, href }: LinkProps) => {
   const label = `Go to ${text}`;
   
   return (
-    <a href={`#${href}`} className="font-medium text-green-600 no-underline relative mr-4 pb-0.5
+    <a href={`#${href}`} className="font-medium text-green-600 no-underline relative pb-0.5
                                     before:w-0 before:absolute before:bottom-0 before:h-0.5 before:bg-red-600 before:transition-width before:ease-in before:duration-200
                                     hover:before:w-full focus:before:w-full">
         {label}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,12 @@ import Link from "./Link";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="flex items-center justify-evenly">
       <Link text="home" href="homeSection" />
       <Link text="about me" href="aboutMeSection" />
+      <svg xmlns="http://www.w3.org/2000/svg" width="100px" height="100px" className="inline-block">
+        <circle cx="50" cy="50" r="50" fill="red" />
+      </svg>
       <Link text="portfolio" href="portfolioSection" />
       <Link text="contact" href="contactSection" />
     </nav>
