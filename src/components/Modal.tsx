be8ddb8f55348623dfa983b6408643e727463a20
Sergiu Pop <sergiupop94@gmail.com>
2022-08-12 16:08:38 +0300

    Integrate headless ui dialog

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,5 @@
-import { PropsWithChildren, useEffect } from "react";
+import { PropsWithChildren } from "react";
+import { Dialog } from "@headlessui/react";
 
 type ModalProps = {
   isOpen: boolean;
@@ -6,22 +7,16 @@ type ModalProps = {
 }
 
 const Modal = ({ children, isOpen, handleClose }: PropsWithChildren<ModalProps>) => {
-  useEffect(() => {
-    const closeOnEscapeKey = (e: KeyboardEvent) => e.key === "Escape" ? handleClose() : null;
-    document.body.addEventListener("keydown", closeOnEscapeKey);
-
-    return () => {
-      document.body.removeEventListener("keydown", closeOnEscapeKey);
-    }
-  }, [handleClose]);
-
-  if (!isOpen) return null;
-
   return (
-    <div className="fixed inset-0 bg-black/60 text-white flex flex-col items-center justify-center overflow-hidden transition-all ease-in-out duration-300 z-[999] pt-10 px-5 pb-5">
-      <button onClick={handleClose}>Close</button>
-      <div className="w-3/4 h-3/4 bg-zinc-700 flex items-center justify-center">{children}</div>
-    </div>
+    <Dialog open={isOpen} onClose={handleClose}>
+      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+
+      <div className="fixed inset-0 flex items-center justify-center p-4">
+        <Dialog.Panel className="mx-auto rounded bg-white">
+          {children}
+        </Dialog.Panel>
+      </div>
+    </Dialog>
   )
 }
 
--- src/helpers/createDivElementInBody.ts
+++
@@ -1,9 +0,0 @@
-const createDivElementInBody = (divElementId: string) => {
-  const divElement = document.createElement("div");
-  divElement.setAttribute("id", divElementId);
-  document.body.appendChild(divElement);
-
-  return divElement;
-};
-
-export default createDivElementInBody;
