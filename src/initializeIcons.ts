53c145bb2891a2f860cacac15bb860d79afbc26b
Sergiu Pop <sergiupop94@gmail.com>
2022-09-05 11:47:27 +0300

    Remove React project

--- src/initializeIcons.ts
+++
@@ -1,8 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faFacebook, faLinkedin, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
-
-const initializeIcons = () => {
-  library.add(faFacebook, faLinkedin, faTwitter, faGithub);
-}
-
-export default initializeIcons;
