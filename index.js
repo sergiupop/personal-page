c40f46a0171e6ed1d45b1419bc6e8a1fd2a1627b
Sergiu Pop <sergiupop94@gmail.com>
2022-09-15 15:17:10 +0300

    Add dark mode

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const activeTheme = "light";
+const activeTheme = "dark";
 const inactiveTheme = activeTheme === "light" ? "dark" : "light";
 const activeThemeIcon = document.getElementById(`${activeTheme}-theme-icon`);
 activeThemeIcon.classList.add("hide-element");
 
 function switchTheme() {
+  document.body.classList.toggle('dark-mode');
   const themeIcons = document.getElementsByClassName("theme-icon");
   for (const themeIcon of themeIcons) {
     themeIcon.classList.toggle("hide-element");
