4cf0eeb8d811eb95f98598389fdae3c62bf091c0
Sergiu Pop <sergiupop94@gmail.com>
2022-11-10 10:18:57 +0200

    Format files and add some responsive design

--- js/scroll.js
+++ js/scroll.js
@@ -3,13 +3,15 @@ const homeSection = document.getElementById("home-section");
 const homeSectionHeight = homeSection.clientHeight;
 
 window.onscroll = () => {
-  if (document.body.scrollTop >= homeSectionHeight || document.documentElement.scrollTop >= homeSectionHeight)
-    scrollToTopLinkContainer.style.display = "flex";
-  else
-    scrollToTopLinkContainer.style.display = "none";
-}
+	if (
+		document.body.scrollTop >= homeSectionHeight ||
+		document.documentElement.scrollTop >= homeSectionHeight
+	)
+		scrollToTopLinkContainer.style.display = "flex";
+	else scrollToTopLinkContainer.style.display = "none";
+};
 
 const scrollToTop = () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+};
--- js/themeSwitch.js
+++ js/themeSwitch.js
@@ -2,40 +2,42 @@ const LIGHT_THEME = "light-theme";
 const DARK_THEME = "dark-theme";
 
 const setThemeClassOnBody = (theme) => {
-  document.body.classList.remove(LIGHT_THEME, DARK_THEME); 
-  document.body.classList.add(theme);
-}
+	document.body.classList.remove(LIGHT_THEME, DARK_THEME);
+	document.body.classList.add(theme);
+};
 
 const resetAllThemeIconsDisplay = () => {
-  const themeIcons = document.getElementsByClassName("theme-icon");
-  for (const themeIcon of themeIcons) {
-    themeIcon.style.display = "none";
-  }
-}
+	const themeIcons = document.getElementsByClassName("theme-icon");
+	for (const themeIcon of themeIcons) {
+		themeIcon.style.display = "none";
+	}
+};
 
 const displayActiveThemeIcon = (theme) => {
-  resetAllThemeIconsDisplay();
-  
-  const activeThemeIcon = document.getElementById(`${theme}-icon`);
-  activeThemeIcon.style.display = "block";
-}
+	resetAllThemeIconsDisplay();
+
+	const activeThemeIcon = document.getElementById(`${theme}-icon`);
+	activeThemeIcon.style.display = "block";
+};
 
 const setNewTheme = (theme) => {
-  setThemeClassOnBody(theme);
-  displayActiveThemeIcon(theme);
-}
+	setThemeClassOnBody(theme);
+	displayActiveThemeIcon(theme);
+};
 
 const setSavedTheme = () => {
-  const savedTheme = localStorage.getItem("page_theme") || LIGHT_THEME;
-  setNewTheme(savedTheme);
-}
+	const savedTheme = localStorage.getItem("page_theme") || LIGHT_THEME;
+	setNewTheme(savedTheme);
+};
 
 setSavedTheme();
 
 const switchTheme = () => {
-  const newTheme = document.body.classList.contains(DARK_THEME) ? LIGHT_THEME : DARK_THEME;
-  localStorage.setItem("page_theme" , newTheme);
-  setNewTheme(newTheme);
-}
+	const newTheme = document.body.classList.contains(DARK_THEME)
+		? LIGHT_THEME
+		: DARK_THEME;
+	localStorage.setItem("page_theme", newTheme);
+	setNewTheme(newTheme);
+};
 
 window.onstorage = setSavedTheme;
